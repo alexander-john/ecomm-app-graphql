9dd75634915eb06857825a7de9069ae7bea3aa1e
Alexander John <96aljohn@gmail.com>
2024-08-04 16:26:37 -0700

    add-data

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const allCourses = [
+    {
+        name: "TypeScript Basics",
+        description: "TypeScript Basics for beginners",
+        price: 599.99,
+        discount: false
+    },
+    {
+        name: "GraphQL Basics",
+        description: "GraphQL Basics for begineers",
+        price: 499.99,
+        discount: true
+    },
+    {
+        name: "NextJS Basics",
+        description: "NextJS Basics for beginners",
+        price: 599.99,
+        discount: false
+    }
+]
+
 const typeDefs = gql `
 type Query {
     courses: [String!]!
