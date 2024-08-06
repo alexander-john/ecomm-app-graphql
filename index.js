8f74edeed3e8bd96277cfef545fb7c20499d535d
Alexander John <96aljohn@gmail.com>
2024-08-05 19:27:45 -0700

    update-index

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { ApolloServer} = require("apollo-server");
 const { typeDefs } = require("./schema");
-const { courses, genres } = require(",/database");
+const { Query } = require("./resolvers/Query");
+const { Course } = require("./resolvers/Course");
+const { Genre } = require("./resolvers/Genre");
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers: 
+    { Query, Course, Genre }});
 
 server.listen().then(({ url}) => console.log(`Server is running at ${url}`));
