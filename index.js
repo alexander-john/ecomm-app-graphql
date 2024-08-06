f729fb8a213397f2d9dc5df6d6690d4e1eec438f
Alexander John <96aljohn@gmail.com>
2024-08-05 19:25:20 -0700

    move-genre-resolver

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ const { ApolloServer} = require("apollo-server");
 const { typeDefs } = require("./schema");
 const { courses, genres } = require(",/database");
 
-const resolvers = {
-    Genre: {
-        courses: (parent, args, context) => {
-            const genreId = parent.id;
-            return courses.filter(item => item.genreId === genreId);
-        }
-    }
-}
-
 const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url}) => console.log(`Server is running at ${url}`));
