e0d3e2426abb7a2801c9a586b3d653ed27b629db
Alexander John <96aljohn@gmail.com>
2024-08-05 19:21:10 -0700

    delete-query-resolver

--- index.js
+++ index.js
@@ -3,35 +3,6 @@ const { typeDefs } = require("./schema");
 const { courses, genres } = require(",/database");
 
 const resolvers = {
-    Query: {
-        courses: () => courses,
-        course: (parent, args, context) => {
-            const courseId = args.id;
-            const course = courses.find(item => item.id ===
-                courseId);
-                if (!course) return null;
-                else return course;
-        },
-        genres: () => genres,
-        genre: (parent, args, context) => {
-            const catId = args.id;
-            const genre = genres.find(item => item.id === catId);
-            if(!genre) return null;
-            else return genre;
-        },
-        welcome: () => {
-            return null;
-        },
-        numOfCourses: () => {
-            return 12;
-        },
-        price: () => {
-            return 1465.98;
-        },
-        isTrainer: () => {
-            return true;
-        }
-    },
     Genre: {
         courses: (parent, args, context) => {
             const genreId = parent.id;
