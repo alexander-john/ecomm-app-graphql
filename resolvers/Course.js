5a7f6e9b8220b5b18154738df62b414370cbff85
Alexander John <96aljohn@gmail.com>
2024-08-05 19:23:51 -0700

    add-course-resolver

--- resolvers/Course.js
+++ resolvers/Course.js
@@ -1,3 +1,8 @@
 const { genres } = require("../database");
 
-exports.Course
+exports.Course = {
+    genre: (parent, args, context) => {
+        const genreId = parent.genreId;
+        return genres.find(item => item.id === genreId);
+    }
+}
