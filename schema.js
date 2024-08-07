e04b1b2b60d7bd313907718476b6343cf1d3a09a
Alexander John <96aljohn@gmail.com>
2024-08-06 19:48:03 -0700

    add-review-schema

--- schema.js
+++ schema.js
@@ -18,10 +18,18 @@ type Course {
     price: Float!
     discount: Boolean!
     genre: Genre
+    reviews: [Review!]!
 }
 type Genre {
     id: ID!
     name: String!
     courses: [Course!]!
+}
+type Review {
+    id: ID!
+    data: String!
+    title: String!
+    comment: String!
+    rating: Int!
 }
 `
