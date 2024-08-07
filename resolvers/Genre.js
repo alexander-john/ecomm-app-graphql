c61c5442f2a5017901de11facf6166ae078a3b25
Alexander John <96aljohn@gmail.com>
2024-08-06 19:35:40 -0700

    add-context

--- resolvers/Genre.js
+++ resolvers/Genre.js
@@ -1,5 +1,6 @@
 exports.Genre = {
     courses: (parent, args, context) => {
+        const courses = context.courses;
         const genreId = parent.id;
         return courses.filter(item => item.genreId === genreId);
     }
