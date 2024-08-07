aa2d17da68ba425904bc8aea3385910cb859ba20
Alexander John <96aljohn@gmail.com>
2024-08-06 19:38:50 -0700

    add-context

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -5,14 +5,19 @@ exports.Query = {
     },
     course: (parent, args, context) => {
         const courseId = args.id;
+        const courses = context.courses;
         const course = courses.find(item => item.id ===
             courseId);
             if (!course) return null;
             else return course;
     },
-    genres: () => genres,
+    genres: (parent, args, context) => {
+        const genres = context.genres;
+        return genres;
+    },
     genre: (parent, args, context) => {
         const catId = args.id;
+        const genres = context.genres;
         const genre = genres.find(item => item.id === catId);
         if(!genre) return null;
         else return genre;
