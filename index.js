2223ebbabc3902129d204e94e46aa6b101bc8081
Alexander John <96aljohn@gmail.com>
2024-08-06 19:44:06 -0700

    import-data

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const { typeDefs } = require("./schema");
 const { Query } = require("./resolvers/Query");
 const { Course } = require("./resolvers/Course");
 const { Genre } = require("./resolvers/Genre");
-const { courses, genres } = require("./database");
+const { courses, genres, reviews } = require("./database");
 
-const server = new ApolloServer({ typeDefs, resolvers: 
-    { Query, Course, Genre }, context: { courses, genres }});
+const server = new ApolloServer({ typeDefs, 
+    resolvers: { Query, Course, Genre }, 
+    context: { courses, genres, reviews }});
 
 server.listen().then(({ url}) => console.log(`Server is running at ${url}`));
