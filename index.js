165d3dff24bf484348ecb1886a537c18fcabafa8
Alexander John <96aljohn@gmail.com>
2024-08-07 21:21:26 -0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { Genre } = require("./resolvers/Genre");
 const { courses, genres, reviews } = require("./database");
 
 const server = new ApolloServer({ typeDefs, 
-    resolvers: { Query, Course, Genre }, 
+    resolvers: { Query, Mutation, Course, Genre }, 
     context: { courses, genres, reviews }});
 
 server.listen().then(({ url}) => console.log(`Server is running at ${url}`));
