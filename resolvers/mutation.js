01a4207d0ee538efe51ed8d35e8ad47954352952
Alexander John <96aljohn@gmail.com>
2024-08-08 20:47:43 -0700

    add-addCourse

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -9,5 +9,15 @@ exports.Mutation = {
         const newGene = { id: uuid(), name }
         genres.push(newGene)
         return newGene
+    }, 
+    addCourse: (parent, args, context) => {
+        const { input } = args;
+        const { name, description, price, discount, genreId } = input;
+        const { courses } = context;
+
+        const newCourse = { id: uuid(), name, description, price,
+            discount, genreId }
+            courses.push(newCourse)
+            return newCourse
     }
 }
