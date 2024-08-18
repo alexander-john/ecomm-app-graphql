dc91716d8398a8611d90d535bb6235cbbe6b01bf
Alexander John <96aljohn@gmail.com>
2024-08-18 11:32:32 -0700

    update-db

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const { Mutation } = require("./resolvers/Mutation");
 const { Query } = require("./resolvers/Query");
 const { Course } = require("./resolvers/Course");
 const { Genre } = require("./resolvers/Genre");
-const { courses, genres, reviews } = require("./database");
+const { db } = require("./database");
 
 const server = new ApolloServer({ typeDefs, 
     resolvers: { Query, Mutation, Course, Genre }, 
-    context: { courses, genres, reviews }});
+    context: { db }});
 
 server.listen().then(({ url}) => console.log(`Server is running at ${url}`));
