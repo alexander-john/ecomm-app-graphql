2b8fbcab03803cc65198d7b17e33cb1343dc97d4
Alexander John <96aljohn@gmail.com>
2024-08-18 12:07:01 -0700

    update-mutation

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -1,31 +1,31 @@
 const { v4: uuid } = require("uuid");
 
 exports.Mutation = {
-    addGenre: (parent, args, context) => {
+    addGenre: (parent, args, { db }) => {
         const { input } = args;
         const { name } = input;
         const { genres } = context;
 
         const newGene = { id: uuid(), name }
-        genres.push(newGene)
+        db.genres.push(newGene)
         return newGene
     }, 
-    addCourse: (parent, args, context) => {
+    addCourse: (parent, args, { db }) => {
         const { input } = args;
         const { name, description, price, discount, genreId } = input;
         const { courses } = context;
 
         const newCourse = { id: uuid(), name, description, price,
             discount, genreId }
-            courses.push(newCourse)
+            db.courses.push(newCourse)
             return newCourse
     },
-    addReview: (parent, args, context) => {
+    addReview: (parent, args, { db }) => {
         const { input } = args;
         const { date, title, comment, rating, courseId } = input;
         const { reviews } = context;
         const newReview = { id: uuid(), date, title, comment, rating, courseId }
-        reviews.push(newReview)
+        db.reviews.push(newReview)
         return newReview
     }
 }
